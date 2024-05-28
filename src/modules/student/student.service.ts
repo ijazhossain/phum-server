087b995783c57773eb41ad9e6ff1f2948201a993
ijazhossain <ijaz.hossain35@gmail.com>
2024-05-28 12:16:01 +0600

    limit query added

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -18,9 +18,9 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     })),
   });
   // Filtering
-  const excludeFields = ['searchTerm', 'sort', 'limit'];
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
   excludeFields.forEach((el) => delete queryObj[el]);
-  // console.log({ query, queryObj });
+  console.log({ query }, { queryObj });
   const filterQuery = searchQuery
     .find(queryObj)
     .populate('admissionSemester')
@@ -35,12 +35,27 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     sort = query.sort as string;
   }
   const sortQuery = filterQuery.sort(sort);
+  let page = 1;
   let limit = 1;
+  let skip = 0;
   if (query.limit) {
-    limit = query.limit as number;
+    limit = Number(query.limit);
   }
-  const limitQuery = await sortQuery.limit(limit);
-  return limitQuery;
+  if (query.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = paginateQuery.limit(limit);
+  // field limiting
+  let fields = '-__v';
+  if (query.fields) {
+    fields = (query.fields as string).split(',').join(' ');
+    // console.log({ fields });
+  }
+  const fieldQuery = await limitQuery.select(fields);
+  return fieldQuery;
 };
 const getSingleStudentFromDB = async (id: string) => {
   if (!(await Student.isUserExists(id))) {
