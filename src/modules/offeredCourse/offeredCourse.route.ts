dd534de8f197cf30292cf60b67d3a026a458fd44
ijazhossain <ijaz.hossain35@gmail.com>
2024-07-02 23:23:02 +0600

    offered course route added

---
+++ src/modules/offeredCourse/offeredCourse.route.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { OfferedCourseControllers } from './offeredCourse.controller';
+
+import { OfferedCourseValidations } from './offeredCourse.validation';
+import validateRequest from '../../app/middlewares/validateRequest';
+
+const router = Router();
+router.post(
+  '/create-offered-course',
+  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
+  OfferedCourseControllers.createOfferedCourse,
+);
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
+  OfferedCourseControllers.updateOfferedCourse,
+);
+router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+router.get('/:id', OfferedCourseControllers.getSingleOfferedCourse);
+router.delete('/:id', OfferedCourseControllers.deleteOfferedCourse);
+export const offeredCourseRoutes = router;
